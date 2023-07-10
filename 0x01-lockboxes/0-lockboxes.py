3cf17b398dd9993c542ea88366610e0fcd8946ed
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-07-10 21:15:14 +0000

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@
 
 
 def canUnlockAll(boxes):
-    '''Checks if all the boxes in a list of boxes containing the keys
+    '''Checks if all the boxes in a list of boxes containing the keys 
     (indices) to other boxes can be unlocked given that the first
     box is unlocked.
     '''
